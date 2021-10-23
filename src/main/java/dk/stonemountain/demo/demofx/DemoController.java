154330b95807350aef626f256c7a6f24727df1ad
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2021-10-23 14:53:23 +0200

    smail changes

--- src/main/java/dk/stonemountain/demo/demofx/DemoController.java
+++ src/main/java/dk/stonemountain/demo/demofx/DemoController.java
@@ -1,5 +1,8 @@
 package dk.stonemountain.demo.demofx;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,19 +10,26 @@ import dk.stonemountain.demo.demofx.about.AboutDialog;
 import dk.stonemountain.demo.demofx.about.IssueDialog;
 import dk.stonemountain.demo.demofx.util.gui.DialogHelper;
 import dk.stonemountain.demo.demofx.util.gui.IconHelper;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Duration;
 
 public class DemoController {
 	private static final String SYSTEM_FAILURE_TITLE = "System Failure";
 	private static final Logger log = LoggerFactory.getLogger(DemoController.class);
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
 	@FXML private Label time;
 	@FXML private BorderPane applicationPane;
+
+	final Timeline timeline = new Timeline();
 
 	@FXML 
 	void doQuit(ActionEvent event) {
@@ -46,6 +56,16 @@ public class DemoController {
 		log.trace("Application Pane center content: {}", applicationPane.getCenter());
 		ImageView imgView = (ImageView) applicationPane.getCenter();
 		applicationPane.setCenter(IconHelper.patchIconPath(imgView));
+		
+		setTime();
+		timeline.setCycleCount(Animation.INDEFINITE);
+//		timeline.setAutoReverse(true);
+		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(5), e -> setTime()));
+		timeline.play();
+	}
+
+	private void setTime() {
+		time.setText(LocalDateTime.now().format(formatter));
 	}
 
 	public void failureOccurred(Throwable e) {
