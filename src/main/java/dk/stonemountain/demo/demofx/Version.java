7491ac262af593bb2161e05e660c007ef917d6c3
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2021-10-23 14:59:48 +0200

    testing version imprint

--- src/main/java/dk/stonemountain/demo/demofx/Version.java
+++ src/main/java/dk/stonemountain/demo/demofx/Version.java
@@ -3,8 +3,9 @@ package dk.stonemountain.demo.demofx;
 public class Version {
 	private Version() {
 		// prevent instantiation
-	}	
-	public static final String APP_VERSION = "V0-0-15_a1";
-	public static final String APP_BUILD_TIME = "11-09-2021 23:45";
+	}
+	
+	public static final String APP_VERSION = "##VERSION##";
+	public static final String APP_BUILD_TIME = "##BUILD_TIME##";
 }
 
