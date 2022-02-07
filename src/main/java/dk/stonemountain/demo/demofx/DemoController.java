0844bfbcab93e114e27032d2a1bf778d1e0454ef
Fin Steenbjerg <fin.steenbjerg@gmail.com>
2022-02-07 21:00:06 +0100

    testing new graalvm version and java17

--- src/main/java/dk/stonemountain/demo/demofx/DemoController.java
+++ src/main/java/dk/stonemountain/demo/demofx/DemoController.java
@@ -18,8 +18,6 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
